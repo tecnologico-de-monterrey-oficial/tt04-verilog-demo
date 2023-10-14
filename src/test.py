de41cee35db40ba83113d95a4b8caacd53008746
matt venn <matt@mattvenn.net>
2023-10-14 17:20:49 +0200

    fixed up test

--- src/test.py
+++ src/test.py
@@ -5,44 +5,34 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
 
-@cocotb.test()
-async def test_7seg(dut):
-    dut._log.info("start")
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
-
+async def check_display(dut, compare):
     # reset
     dut._log.info("reset")
     dut.rst_n.value = 0
     # set the compare value
-    dut.ui_in.value = 1
+    dut.ui_in.value = compare
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
     # the compare value is shifted 10 bits inside the design to allow slower counting
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
+    max_count = compare << 10
+    dut._log.info(f"check all segments with compare set to {compare} [{max_count} cycles]")
     # check all segments and roll over
     for i in range(15):
-        dut._log.info("check segment {}".format(i))
+        dut._log.info(f"check segment {i%10}")
         await ClockCycles(dut.clk, max_count)
         assert int(dut.segments.value) == segments[i % 10]
 
         # all bidirectionals are set to output
         assert dut.uio_oe == 0xFF
 
-    # reset
-    dut.rst_n.value = 0
-    # set a different compare value
-    dut.ui_in.value = 3
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+@cocotb.test()
+async def test_7seg(dut):
+    dut._log.info("start")
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
 
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
+    await check_display(dut, 1)
+    await check_display(dut, 5)
+    await check_display(dut, 10)
 
